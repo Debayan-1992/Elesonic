e94b31a431f6282450c1873ac8c6749bf28761bf
debo2696 <debo2696@gmail.com>
2022-05-09 17:25:49 +0530

    pass_upd customer,seller

---
+++ resources/views/frontend/dashboard/password_update_form.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.frontend.app')
+
+@section('content')
+<div class="container">
+    <div class="main-body">
+        <div class="row">
+            <div class="col-lg-11">
+                <div class="card">
+                    <div class="card-body">
+                        @if(session()->has('message'))
+                            <div class="alert alert-success">
+                                {{ session()->get('message') }}
+                            </div>
+                        @endif
+
+                        @if($errors->any())
+                        <div class="alert alert-danger">
+                            {!! implode('', $errors->all('<div>:message</div>')) !!}
+                        </div>
+                        @endif
+                        
+                        @if($user->role_id  == App\Model\Role::IS_CUSTOMER)
+                        <form action="{{route('customer.frontend_pass_upd')}}" method="post" id="pass_form">
+                        @endif
+                        @if($user->role_id  == App\Model\Role::IS_SELLER)
+                        <form action="{{route('seller.frontend_pass_upd')}}" method="post" id="pass_form">
+                        @endif
+                            @csrf
+                            <input type="text" name="user_id" class="form-control" value="{{encrypt($user->id)}}" hidden>
+                            <div class="row mb-3">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">Password</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    <input type="text" name="password" class="form-control" value="">
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">Re-enter Password</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    <input type="text" name="password_confirmation" class="form-control" value="">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-3"></div>
+                                <div class="col-sm-9 text-secondary">
+                                    <input type="submit" class="btn btn-primary px-4" value="Save Changes">
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </dIv>
+        </dIv>
+    </dIv>
+</dIv>
+@endsection
